13f683ff94e0aace84c973e4468826127cab670c
Dima Gorbenko <dmitriy.gorbenko@gmail.com>
2018-01-11 18:39:29 +0200

    adding initial code

---
+++ index.js
@@ -0,0 +1,103 @@
+
+'use strict';
+
+var
+    _ = require("lodash"),
+    fs = require("fs"),
+    path = require("path");
+
+// instead of importing colors I used this article:
+// https://ourcodeworld.com/articles/read/298/how-to-show-colorful-messages-in-the-console-in-node-js
+
+function logInfo() {
+
+    var
+        args = Array.prototype.slice.call(arguments);
+
+    args.unshift("\x1b[1m\x1b[37m");
+    args.push("\x1b[39m\x1b[22m");
+
+    console.log.apply(console, args);
+}
+
+function logError() {
+    var
+        args = Array.prototype.slice.call(arguments);
+
+    args.unshift("\x1b[1m\x1b[31m");
+    args.push("\x1b[39m\x1b[22m");
+
+    console.log.apply(console, args);
+}
+
+function ConfigPlugin(options) {
+
+    this.options = _.defaults(options, {
+        modulesToImportConfigFrom: []
+    });
+};
+
+ConfigPlugin.prototype.apply = function(compiler) {
+
+    compiler.plugin("environment", function() {
+
+        var
+            importedAliases = {},
+            webpackAliases = compiler.options.resolve.alias;
+
+        logInfo("Merging config files from different modules if possible...");
+
+        _.each(this.options.modulesToImportConfigFrom, function (moduleName) {
+
+            logInfo("Merging config from: ", moduleName);
+
+            var
+                config,
+                moduleConfigFileName = path.normalize(compiler.options.context + "/bower_components/" + moduleName + "/config.js");
+
+            if (!fs.existsSync(moduleConfigFileName)) {
+                logError("Config file form module '" + moduleName + "' doesn't exists: " + moduleConfigFileName);
+                return;
+            }
+
+            logInfo("... getting config data from " + moduleConfigFileName);
+
+            var content = fs.readFileSync(moduleConfigFileName);
+
+            var
+                functToGetConfig = new Function("myFunc", "var\n" +
+                    "        config,\n" +
+                    "        requirejs = {\n" +
+                    "            config: function (config) {\n" +
+                    "                return config;\n" +
+                    "            }\n" +
+                    "        };\n" +
+                    "    \n" +
+                    "    config = " + content + " return config;")
+
+            try {
+                config = functToGetConfig();
+            } catch (e) {
+                logError("... failed to get from this file, please, check it");
+                return;
+            }
+
+            if (!config.webpackConfig) {
+                return;
+            }
+
+            if (config.webpackConfig.resolve) {
+                if (config.webpackConfig.resolve.alias) {
+                    importedAliases[moduleName] = config.webpackConfig.resolve.alias;
+                }
+            }
+        });
+
+        _.each(importedAliases, function (aliases, moduleName) {
+            webpackAliases = _.extend(webpackAliases, aliases);
+        });
+
+    }.bind(this));
+};
+
+module.exports = ConfigPlugin;
