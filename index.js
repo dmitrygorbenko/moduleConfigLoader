3efbe2f7292f34710fc62ae92eeada12cfe6b16b
Dima Gorbenko <dmitriy.gorbenko@gmail.com>
2018-01-17 14:24:55 +0200

    some refactoring

--- index.js
+++ index.js
@@ -6,104 +6,146 @@ var
     fs = require("fs"),
     path = require("path");
 
-// instead of importing colors I used this article:
-// https://ourcodeworld.com/articles/read/298/how-to-show-colorful-messages-in-the-console-in-node-js
-
-function logInfo() {
-
-    var
-        args = Array.prototype.slice.call(arguments);
-
-    args.unshift("\x1b[1m\x1b[37m");
-    args.push("\x1b[39m\x1b[22m");
-
-    console.log.apply(console, args);
-}
-
-function logError() {
-    var
-        args = Array.prototype.slice.call(arguments);
-
-    args.unshift("\x1b[1m\x1b[31m");
-    args.push("\x1b[39m\x1b[22m");
-
-    console.log.apply(console, args);
-}
-
 function ConfigPlugin(options) {
 
     this.options = _.defaults(options, {
-        modulesToImportConfigFrom: []
+        verbose: false,
+        modulesToImportAliasesFrom: []
     });
 };
 
-ConfigPlugin.prototype.apply = function(compiler) {
+ConfigPlugin.prototype = _.extend(ConfigPlugin.prototype, {
 
-    compiler.plugin("environment", function() {
+    apply: function (compiler) {
 
-        var
-            compiledAliasesList = {},
-            importedAliases = {};
-
-        logInfo("Merging config files from different modules if possible...");
-
-        _.each(this.options.modulesToImportConfigFrom, function (moduleName) {
-
-            logInfo("Merging config from: ", moduleName);
+        compiler.plugin("environment", function() {
 
             var
-                config,
+                aliasesOfThisModule,
+                compiledAliasesList = {};
+
+            this.logInfo("Merging aliases files from different modules if possible...");
+
+            _.each(this.options.modulesToImportAliasesFrom, function (moduleName) {
+                var
+                    aliases,
+                    moduleConfigFileName;
+
                 moduleConfigFileName = path.normalize(compiler.options.context + "/bower_components/" + moduleName + "/config.js");
 
-            if (!fs.existsSync(moduleConfigFileName)) {
-                logError("Config file form module '" + moduleName + "' doesn't exists: " + moduleConfigFileName);
-                return;
-            }
+                aliases = this.readAliasesFromModule({
+                    compiler: compiler,
+                    moduleName: moduleName,
+                    moduleConfigFileName: moduleConfigFileName
+                });
 
-            logInfo("... getting config data from " + moduleConfigFileName);
-
-            var content = fs.readFileSync(moduleConfigFileName);
-
-            var
-                functToGetConfig = new Function("myFunc", "var\n" +
-                    "        config,\n" +
-                    "        requirejs = {\n" +
-                    "            config: function (config) {\n" +
-                    "                return config;\n" +
-                    "            }\n" +
-                    "        };\n" +
-                    "    \n" +
-                    "    config = " + content + " return config;")
-
-            try {
-                config = functToGetConfig();
-            } catch (e) {
-                logError("... failed to get from this file, please, check it");
-                return;
-            }
-
-            if (!config.webpackConfig) {
-                return;
-            }
-
-            if (config.webpackConfig.resolve) {
-                if (config.webpackConfig.resolve.alias) {
-                    importedAliases[moduleName] = config.webpackConfig.resolve.alias;
-
-                    logInfo("... read", Object.keys(config.webpackConfig.resolve.alias).length, "alises");
+                if (!aliases) {
+                    return;
                 }
+
+                compiledAliasesList = _.extend(compiledAliasesList, aliases);
+
+            }.bind(this));
+
+
+            // now, get aliases from our own module
+            aliasesOfThisModule = this.readAliasesFromModule({
+                compiler: compiler,
+                moduleName: this.options.thisModuleName,
+                moduleConfigFileName: path.normalize(compiler.options.context + "/../config.js")
+            });
+            if (aliasesOfThisModule) {
+                compiledAliasesList = _.extend(compiledAliasesList, aliasesOfThisModule);
             }
-        });
 
-        _.each(importedAliases, function (aliases, moduleName) {
-            compiledAliasesList = _.extend(compiledAliasesList, aliases);
-        });
 
-        compiler.options.resolve.alias = _.extend(compiledAliasesList, compiler.options.resolve.alias);
+            //, now, put all read aliases back to webpack configuration
+            compiler.options.resolve.alias = _.extend(compiledAliasesList, compiler.options.resolve.alias);
 
-        //logInfo("Aliases are: ", compiler.options.resolve.alias);
+            if (this.options.verbose) {
+                this.logInfo("\nCompiled webpack aliases are:\n", compiler.options.resolve.alias, "\n");
+            }
 
-    }.bind(this));
-};
+        }.bind(this));
+    },
+
+    readAliasesFromModule: function (options) {
+
+        var
+            aliases = null,
+            content,
+            config,
+            functToGetConfig,
+
+            compiler = options.compiler,
+            moduleName = options.moduleName,
+            moduleConfigFileName = options.moduleConfigFileName;
+
+        this.logInfo("Reading webpack aliases (config file is 'config.js') from module", moduleName);
+
+        if (!fs.existsSync(moduleConfigFileName)) {
+            this.logError("Config file from module '" + moduleName + "' doesn't exists: " + moduleConfigFileName);
+            return aliases;
+        }
+
+        this.logInfo("... getting config data from " + moduleConfigFileName);
+
+        content = fs.readFileSync(moduleConfigFileName);
+
+        functToGetConfig = new Function("myFunc", "var\n" +
+            "        config,\n" +
+            "        requirejs = {\n" +
+            "            config: function (config) {\n" +
+            "                return config;\n" +
+            "            }\n" +
+            "        };\n" +
+            "    \n" +
+            "    config = " + content + " return config;")
+
+        try {
+            config = functToGetConfig();
+        } catch (e) {
+            this.logError("... failed to get config data from this file, please, check it");
+            return aliases;
+        }
+
+        if (!config.webpackConfig) {
+            return aliases;
+        }
+
+        if (config.webpackConfig.resolve) {
+            if (config.webpackConfig.resolve.alias) {
+                aliases = config.webpackConfig.resolve.alias;
+
+                this.logInfo("... read", Object.keys(aliases).length, "alises");
+            }
+        }
+
+        return aliases;
+    },
+
+    // instead of importing colors I used this article:
+    // https://ourcodeworld.com/articles/read/298/how-to-show-colorful-messages-in-the-console-in-node-js
+    logInfo: function () {
+
+        var
+            args = Array.prototype.slice.call(arguments);
+
+        args.unshift("\x1b[1m\x1b[37m");
+        args.push("\x1b[39m\x1b[22m");
+
+        console.log.apply(console, args);
+    },
+
+    logError: function () {
+        var
+            args = Array.prototype.slice.call(arguments);
+
+        args.unshift("\x1b[1m\x1b[31m");
+        args.push("\x1b[39m\x1b[22m");
+
+        console.log.apply(console, args);
+    }
+})
 
 module.exports = ConfigPlugin;
